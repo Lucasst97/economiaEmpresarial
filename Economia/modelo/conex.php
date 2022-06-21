b55852cea825f28b3508aeacae460db0cbeb5d55
Lucasst97 <57073773+Lucasst97@users.noreply.github.com>
2022-06-20 21:51:52 -0300

    Update conex.php
    actualizacion parametros de conexion a BD

--- Economia/modelo/conex.php
+++ Economia/modelo/conex.php
@@ -1,6 +1,6 @@
 <?php
 
-$conex = mysqli_connect("localhost", "root", "", "economia_empresarial");
+$conex = mysqli_connect("localhost", "id19095293_usereconomia", "05}<AAsjPQyLfQMV", "id19095293_economiaempresarial");
 
 
 ?>
