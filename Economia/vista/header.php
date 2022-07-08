f0f051e2fda5c513e7de919ab7129d55454ee8de
Luciana <74796804+Piiiiri@users.noreply.github.com>
2022-07-08 18:39:44 -0300

    Nueva Vista
    Se agregan header, footer y menu por separado.

---
+++ Economia/vista/header.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <style>
+    body {
+      font-family: 'Montserrat', sans-serif;
+      background-image: url('elements/background.jpg');
+      background-repeat: no-repeat;
+      background-position: center;
+      background-size: auto;
+    }
+
+    .menu-contable:hover {
+      color: black;
+      text-decoration: underline;
+    }
+
+    .card {
+      border: none;
+      width: 300px;
+      height: 450px;
+    }
+  </style>
+  <title>Economia Empresarial</title>
+</head>
---
+++ Economia/vista/menu.php
@@ -0,0 +1,39 @@
+<body>
+  <nav class="navbar navbar-expand-lg " style="background-color: #284078;">
+    <div class="container-fluid align-self-center">
+      <a class="navbar-brand d-block text-light " href="#">
+        <img src="elements/economy.png" alt="" width="30" height="30" class="d-inline-block align-text-top me-2">
+        Estudio Contable
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse position-absolute end-0" id=" navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 pull-xs-left">
+          <li class="nav-item">
+            <a class="nav-link active text-light menu-contable" style="padding-right: 30px" aria-current="page" href="#">Inicio</a>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link text-light menu-contable" style="padding-right: 30px" href="#">Plan de Cuentas
+            </a>
+          </li>
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-light menu-contable" style="padding-right: 30px" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Balance
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item menu-contable" href="#">Nuevo Balance</a></li>
+              <li><a class="dropdown-item menu-contable" href="#">Hitorial</a></li>
+            </ul>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link text-light menu-contable" style="padding-right: 30px">Libro Diario</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
