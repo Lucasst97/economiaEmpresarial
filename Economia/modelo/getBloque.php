8d3f5e60ada98669cdce4187ace38a22b4df5733
Lucasst97 <57073773+Lucasst97@users.noreply.github.com>
2022-07-10 17:18:26 -0300

    actualizacion getBloque
    actualizacion getBloque

--- Economia/modelo/getBloque.php
+++ Economia/modelo/getBloque.php
@@ -2,14 +2,14 @@
 	include ('conex.php');
 	
 	$cod_grupo = $_POST['cod_grupo'];
-    echo $cod_grupo;
+    //echo $cod_grupo;
 	
 	$queryB = "SELECT cod_bloque, nombre_bloque FROM bloque WHERE cod_grupo = '$cod_grupo'";
-    echo $queryB.'...';
+    //echo $queryB.'...';
     $rsB = mysqli_query($conex, $queryB);
-	if ($rsB) {
+	/* if ($rsB) {
         echo 'ok';
-    }
+    } */
 	$html= "<option value='0'>Seleccionar Bloque</option>";
 	
 	while($rowB = mysqli_fetch_assoc($rsB)){
