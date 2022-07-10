f728ace889fd6e68af492fbb92ad37f5540d52c4
Lucasst97 <57073773+Lucasst97@users.noreply.github.com>
2022-07-10 17:25:32 -0300

    Incorporacion insertar cuenta
    Se incorporo funcionalidad de insertar nueva cuenta

---
+++ Economia/modelo/ingresarCuenta.php
@@ -0,0 +1,23 @@
+<?php
+include ('conex.php');
+
+$cod_grupo = $_POST['cod_grupo'];
+$cod_bloque = $_POST['cod_bloque'];
+$cod_rubro = $_POST['cod_rubro'];
+$nombre_cuenta = $_POST['grupoCuenta'];
+
+$queryC = "SELECT MAX(cod_cuenta) FROM cuenta WHERE cod_rubro = '$cod_rubro' and cod_bloque= '$cod_bloque' and cod_grupo= '$cod_grupo' ";
+$rsC = mysqli_query($conex, $queryC);
+
+$rowC = mysqli_fetch_assoc($rsC);
+
+$codCuenta = $rowC['MAX(cod_cuenta)'];
+$codCuenta = $codCuenta+1;
+
+$queryI= "INSERT INTO `cuenta`(`cod_cuenta`, `nombre_cuenta`, `cod_rubro`, `cod_bloque`, `cod_grupo`) VALUES ('$codCuenta','$nombre_cuenta','$cod_rubro','$cod_bloque','$cod_grupo')";
+echo $queryI;
+$insert = mysqli_query($conex, $queryI)
+
+
+
+?>
