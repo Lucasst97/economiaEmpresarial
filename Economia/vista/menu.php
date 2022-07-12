7bc93b58079fef825313f9e4c6fe7149c1fec973
Lucasst97 <57073773+Lucasst97@users.noreply.github.com>
2022-07-12 02:13:55 -0300

    Se agregaron comentarios y se limpio el codigo
    Se agregaron comentarios y se limpio el codigo

--- Economia/vista/menu.php
+++ Economia/vista/menu.php
@@ -1,7 +1,7 @@
 <body>
   <nav class="navbar navbar-expand-lg " style="background-color: #284078;">
     <div class="container-fluid align-self-center">
-      <a class="navbar-brand d-block text-light " href="#">
+      <a class="navbar-brand d-block text-light " href="index.php">
         <img src="elements/economy.png" alt="" width="30" height="30" class="d-inline-block align-text-top me-2">
         Estudio Contable
       </a>
@@ -11,12 +11,17 @@
       <div class="collapse navbar-collapse position-absolute end-0" id=" navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0 pull-xs-left">
           <li class="nav-item">
-            <a class="nav-link active text-light menu-contable" style="padding-right: 30px" aria-current="page" href="#">Inicio</a>
+            <a class="nav-link active text-light menu-contable" style="padding-right: 30px" aria-current="page" href="index.php">Inicio</a>
           </li>
 
-          <li class="nav-item">
-            <a class="nav-link text-light menu-contable" style="padding-right: 30px" href="#">Plan de Cuentas
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle text-light menu-contable" style="padding-right: 30px" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Plan de Cuentas
             </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item menu-contable" href="cuentas.php">Ingresar Cuenta</a></li>
+              <li><a class="dropdown-item menu-contable" href="tabla_valores.php">Descargar</a></li>
+            </ul>
           </li>
 
           <li class="nav-item dropdown">
