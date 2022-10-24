a84f8545a0179abd6221b3fc13a384e50ec2b9a8
Luciana <74796804+Piiiiri@users.noreply.github.com>
2022-10-24 19:32:15 -0300

    Delete Economia directory

--- Economia/modelo/conex.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// $conex = mysqli_connect("localhost", "economia20", "Economia20", "economia20");
-$conex = mysqli_connect("localhost", "root", "", "economia20");
-
-?>
--- Economia/modelo/insertar_libro_diario.php
+++
@@ -1,6 +0,0 @@
-<?php
-$cuenta_asiento = $_POST['cuenta_insert'];
-$detalle_asiento = $_POST['detalle_cuenta'];
-$saldo_asiento = $_POST['saldo_cuenta'];
-$saldo_haber = $_POST['option_debe'];
-$saldo_haber = $_POST['option_haber'];
