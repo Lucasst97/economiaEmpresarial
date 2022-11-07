a999860092be7b14e20682e2cddf6a809a248413
laralvarez <73253822+laralvarez@users.noreply.github.com>
2022-11-07 18:36:46 -0300

    Modificación texto y formato

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $selectcuentastotales = selectTotalGrupos();
         <!-- ========================Contenido de Index========================= -->
         <div class="row">
           <!-- ========================Titulo del Sitio========================= -->
-          <h1 style="color:#FFF; text-align: center; margin-left: auto; margin-right: auto; padding: 20px ; ">Economia Empresarial</h1>
+          <h1 style="color:#FFF; text-align: center; margin-left: auto; margin-right: auto; padding: 20px ; ">Economía Empresarial</h1>
 
           <div class="col-6" style="padding:20px; margin: auto; width: 100%; background-color:#FFF">
             <div style="justify-content: center; padding:auto; width:80%; margin-left:auto; margin-right:auto;">
@@ -23,18 +23,10 @@ $selectcuentastotales = selectTotalGrupos();
 
 
             <div style="padding:25px; font-size: 18px;">
-              <p style="text-indent: 20px">Este Sitio Web esta siendo desarrollado por estudiantes de 3ro de la carrera Tecnicatura Superio en Analisis, 
-                Desarrollo y Pogramacion de aplicaciones para la instancia evaluativa de la materia Economia 
-                empresarial.</p>
-              <p style="text-indent: 20px">Actualmente, se incorporo al sistema la seccion "Plan de Cuentas" donde la empresa puede visualizar 
-              todas las cuentas cargadas en dicho plan e imprimirlas  en formato PDF en "Descargar cuentas". Estas 
-              cuentas pueden ser eliminadas mediante un boton solo si la cuenta posee saldo 0. Ademas, puede 
-              modificarse el nombre de las mismas e ingresar nuevas al plan seleccionando previamente el Grupo, Bloque
-              y Rubro al cual pertenecera dicha cuenta.
-              </p>
-              <p style="text-indent: 20px">
-              Esperamos que cumpla las expectativas de la catedra y que pueda ser util en un futuro proximo.
-              </p>
+              <p style="text-justify: 20px">Este Sitio Web está siendo desarrollado por estudiantes de 3ro de la carrera Tecnicatura Superior en Análisis, Desarrollo y Programación de aplicaciones para la instancia evaluativa de la materia “Economía empresarial”.</p>
+              <p style="text-justify: 20px">En primer lugar, se incorporó al sistema la sección "Plan de Cuentas" donde la empresa puede visualizar todas las cuentas cargadas en dicho plan e imprimirlas en formato PDF en "Descargar cuentas". Estas cuentas pueden ser eliminadas mediante un botón solo si la cuenta posee saldo 0. Además, puede modificarse el nombre de las mismas e ingresar nuevas al plan seleccionando previamente el Grupo, Bloque y Rubro al cual pertenecerá dicha cuenta.</p>
+              <p style="text-justify: 20px">Seguidamente, agregamos la opción “Libro Diario” en la cual podemos añadir un nuevo asiento. Pudiendo elegir que cuentas vamos a utilizar y si el saldo pertenece al Debe o al Haber con su respectivo detalle. Visualizando los datos posteriormente.</p>
+              <p style="text-justify: 20px">Esperamos que cumpla las expectativas de la catedra y que pueda ser útil en un futuro próximo.</p>
             </div>
             
           </div>
